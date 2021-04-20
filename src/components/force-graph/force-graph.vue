a110d9015e98e5b1d4de28a655dbb5f6861692c0
ZephyrTan <910622082@qq.com>
2021-04-20 08:21:56 +0800

    新增

--- src/components/force-graph/force-graph.vue
+++ src/components/force-graph/force-graph.vue
@@ -4,6 +4,7 @@
 </template>
 <script lang="ts" setup>
 import * as d3 from 'd3'
+import {onMounted} from "vue";
 
 let data = {
   "nodes": [
@@ -344,12 +345,14 @@ let data = {
 }
 const links = data.links.map(d => Object.create(d));
 const nodes = data.nodes.map(d => Object.create(d));
-let width = 800
-let height = 800
+const width = 800
+const height = 800
+let [simulation, svg, link, node] = [null]
 
-const color = () => {
+const color = d => {
   const scale = d3.scaleOrdinal(d3.schemeCategory10);
-  return d => scale(d.group);
+  console.log(scale(d.group), d.group)
+  return scale(d.group);
 }
 
 const drag = simulation => {
@@ -377,39 +380,49 @@ const drag = simulation => {
       .on("end", dragended);
 }
 
-const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+const createSimulation = () => {
+  simulation = d3.forceSimulation(nodes)
+      .force("link", d3.forceLink(links).id(d => d.id))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2));
+  simulation.on("tick", () => {
+    link.attr("x1", d => d.source.x).attr("y1", d => d.source.y).attr("x2", d => d.target.x).attr("y2", d => d.target.y);
+    node.attr("cx", d => d.x).attr("cy", d => d.y);
+  });
+}
+const createSvg = () => {
+  svg = d3.select('#graph').append("svg")
+      .attr("viewBox", [0, 0, width, height]);
+}
 
-const svg = d3.select('#graph').append("svg")
-    .attr("viewBox", [0, 0, width, height]);
+const addLinks = () => {
+  link = svg.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(links)
+      .join("line")
+      .attr("stroke-width", d => Math.sqrt(d.value));
+}
 
-const link = svg.append("g")
-    .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line")
-    .attr("stroke-width", d => Math.sqrt(d.value));
-
-const node = svg.append("g")
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-    .attr("r", 5)
-    .attr("fill", color)
-    .call(drag(simulation));
-
-node.append("title")
-    .text(d => d.id);
-
-simulation.on("tick", () => {
-  link.attr("x1", d => d.source.x).attr("y1", d => d.source.y).attr("x2", d => d.target.x).attr("y2", d => d.target.y);
-  node.attr("cx", d => d.x).attr("cy", d => d.y);
-});
+const addNodes = () => {
+  node = svg.append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+      .attr("r", 5)
+      .attr("fill", color)
+      .call(drag(simulation));
+  node.append("title")
+      .text(d => d.id);
+}
+onMounted(() => {
+  createSimulation();
+  createSvg()
+  addNodes()
+  addLinks()
+})
 </script>
-
 <style src="./force-graph.scss" lang="scss" scoped></style>
